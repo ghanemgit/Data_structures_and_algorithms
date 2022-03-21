c5142474c5c6ce6dd789c3258d5288994850f38c
ghanemgit <ghanem_97@outlook.com>
2022-03-21 23:25:02 +0300

    Fix Challenge11

---
+++ app/src/main/java/CodeChallenges/Challenge12/AnimalShelter.java
@@ -0,0 +1,23 @@
+package CodeChallenges.Challenge12;
+
+
+public class AnimalShelter {
+
+
+    public AnimalShelter() {
+    }
+
+
+
+
+    public void enqueue(Animals animal){
+
+//        if(animal.equals())
+//        //AnimalShelter animalShelter = new AnimalShelter();
+//        //animalShelter.d
+    }
+
+
+
+
+}
