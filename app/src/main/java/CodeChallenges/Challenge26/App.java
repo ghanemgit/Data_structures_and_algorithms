14337c47f5232e33eb5068f93a1defb3b9e71343
ghanemgit <ghanem_97@outlook.com>
2022-04-26 15:09:04 +0300

    Add visual process and more test cases

--- app/src/main/java/CodeChallenges/Challenge26/App.java
+++ app/src/main/java/CodeChallenges/Challenge26/App.java
@@ -6,14 +6,22 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
 
-        int[] arr = {8,4,23,42,16,15};
+        
+        int[] arr = {8, 4, 23, 42, 16, 15};
+        int[] arr2 = {20, 18, 12, 8, 5, -2};
+        int[] arr3 = {5, 12, 7, 5, 5, 7};
+        int[] arr4 = {2, 3, 5, 7, 13, 11};
 
         System.out.println(Arrays.toString(insertionSort(arr)));
+        System.out.println(Arrays.toString(insertionSort(arr2)));
+        System.out.println(Arrays.toString(insertionSort(arr3)));
+        System.out.println(Arrays.toString(insertionSort(arr4)));
+
 
 
     }
 
-    public static int[] insertionSort(int[] arr){
+    public static int[] insertionSort(int[] arr) {
 
 
         for (int i = 1; i < arr.length; ++i) {
