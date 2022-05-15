b9dc76ebb8e5732a5364be2cb3d66f17177f333d
ghanemgit <ghanem_97@outlook.com>
2022-05-16 01:24:36 +0300

    challenge solved

--- app/README/README_Ch31.md
+++ app/README/README_Ch31.md
@@ -3,12 +3,14 @@
 
 
 ## Whiteboard Process
-![WhiteBoard](../../Assert/Ch31T.png)
+![WhiteBoard](../../Assert/Ch31F.png)
 
 ## Approach & Efficiency
 - Split the string and add each word in an array.
-- Iterate through the array and convert the word to lower case, then check if the word has a special char then remove it.
+- Iterate through the array and convert the word to lower case, then check if the word has a special character then remove it.
 - If the hashtable not contains the word add each word and the count of repeat it in hashtable, else return this word because that means the word is repeated.
 - The Big O space O(n) / time is O(n).
+
+
 ## Solution
 ### Go [here](../src/main/java/CodeChallenges/Challenge31/App.java) and check my code and tun it in IDE.
--- app/src/main/java/CodeChallenges/Challenge31/App.java
+++ app/src/main/java/CodeChallenges/Challenge31/App.java
@@ -1,27 +1,15 @@
 package CodeChallenges.Challenge31;
 
 import CodeChallenges.Challenge30.HashTable;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+
 
 
 public class App {
 
     public static void main(String[] args) {
 
-        String string = "It was the best of times, it was the worst of times," +
-                " it was the age of wisdom, it was the age of foolishness, it was " +
-                "the epoch of belief, it was the epoch of incredulity, it was the " +
-                "season of Light, it was the season of Darkness, it was the spring " +
-                "of hope, it was the winter of despair, we had everything before us," +
-                " we had nothing before us, we were all going direct to Heaven, " +
-                "we were all going direct the other way – in short, the period was so" +
-                " far like the present period, that some of its noisiest authorities" +
-                " insisted on its being received, for good or for evil, in the superlative" +
-                " degree of comparison only...";
+        String string = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...";
         System.out.println("The repeated word is => "+repeatedWord(string));
 
     }
@@ -30,27 +18,33 @@ public class App {
     public static String repeatedWord(String str) {
 
         //This split to mark the space between the words and to help us to dividing it
-        String[] wordsArr = str.toLowerCase().split(" ");
-
+        String[] wordsArr = str.toLowerCase().replaceAll("[^a-zA-Z’]+"," ").split(" ");
+        System.out.println(Arrays.toString(wordsArr));
         // hashTable for storing word and its count
         HashTable<String, Integer> wordsHashTable = new HashTable<>();
 
         // store all the words of string and the count of word in hashTable
-        for (String string : wordsArr) {
-            if (wordsHashTable.contain(string))
-                wordsHashTable.add(string, wordsHashTable.get(string) + 1);
-            else
-                wordsHashTable.add(string, 1);
-        }
+        for (int i=0; i<wordsArr.length; i++) {
 
-         //traverse again from first word of string str to check if count of word is greater than 1
-         //either take a new stream or store the words in vector of strings in previous loop
-        for (String string : wordsArr) {
-            int count = wordsHashTable.get(string);
-            if (count > 1) {
-                return string;
+            //check if the word already exist in hash table
+            if (wordsHashTable.contain(wordsArr[i])){
+
+                //if exist add the same word to tha hash table and increase the value by one
+                wordsHashTable.add(wordsArr[i], wordsHashTable.get(wordsArr[i]) + 1);
+
+                //at the same time assign the value of that word in count variable
+                int count = wordsHashTable.get(wordsArr[i]);
+
+                //if the count more than one that mean the word is repeated so return the that's key
+                if (count > 1) {
+                    return wordsArr[i];
+                }
             }
+            //else the word does not exist add it normally to the table
+            else
+                wordsHashTable.add(wordsArr[i], 1);
         }
+        //if there is no repeated words return this string
         return "No Repetition";
     }
 }
